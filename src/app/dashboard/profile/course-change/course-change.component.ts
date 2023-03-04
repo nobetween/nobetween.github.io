f8fdff381aab68983cde6da6d71b269bd44bde86
cbriggs84 <cbriggs84@aol.com>
2023-03-04 09:53:23 -0500

    styles and worked on change course comp

--- src/app/dashboard/profile/course-change/course-change.component.ts
+++ src/app/dashboard/profile/course-change/course-change.component.ts
@@ -1,21 +1,54 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormControl, ReactiveFormsModule, UntypedFormGroup } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatOptionModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { NavService } from 'src/app/services/nav-service';
 
 @Component({
 	standalone: true,
 	selector: 'app-course-change',
 	templateUrl: './course-change.component.html',
 	styleUrls: ['./course-change.component.scss', '../../dashboard.component.scss'],
-	imports: [CommonModule],
+	imports: [CommonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatSelectModule, MatOptionModule],
 	encapsulation: ViewEncapsulation.None
 })
 export class CourseChangeComponent implements OnInit
 {
+	formGroup = new UntypedFormGroup({});
 
-	constructor() { }
+	vmCourse = [
+		{ label: "Course 1", value: "Course 1" },
+		{ label: "Course 2", value: "Course 2" },
+		{ label: "Course 3", value: "Course 3" },
+	];
+
+	vmAcess = [
+		{ label: "Access 1", value: "Access 1" },
+		{ label: "Access 2", value: "Access 2" },
+		{ label: "Access 3", value: "Access 3" }
+	];
+
+	constructor(private navService: NavService) { }
 
 	ngOnInit(): void
 	{
+		this.formGroup.addControl('Course', new FormControl());
+		this.formGroup.addControl('ClubCode', new FormControl());
+		this.formGroup.addControl('AccessLevel', new FormControl());
+	}
+
+	onUpdate_Click()
+	{
+		this.navService.navigateToPreviousURL();
+	}
+
+	onAdd_Click()
+	{
+		this.navService.navigateToPreviousURL();
 	}
 
 }
