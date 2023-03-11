2169fbf86981c627f2cbd7cdd7ef09eb40b1140a
cbriggs84 <cbriggs84@aol.com>
2023-03-10 21:13:28 -0500

    tee location comp

--- src/app/dashboard/couse-club-layout/tee-location/tee-location.component.ts
+++ src/app/dashboard/couse-club-layout/tee-location/tee-location.component.ts
@@ -1,21 +1,52 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ReactiveFormsModule, UntypedFormGroup, UntypedFormControl } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatOptionModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule, MatSelectChange } from '@angular/material/select';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { Course, CourseService } from 'src/app/services/course-service';
+import { TeeLocation, TeeService } from 'src/app/services/tee-service';
 
 @Component({
 	standalone: true,
 	selector: 'app-tee-location',
 	templateUrl: './tee-location.component.html',
 	styleUrls: ['./tee-location.component.scss', '../../dashboard.component.scss'],
-	imports: [CommonModule],
+	imports: [CommonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatOptionModule, MatTableModule, MatButtonModule],
 	encapsulation: ViewEncapsulation.None
 })
 export class TeeLocationComponent implements OnInit
 {
+	formGroup = new UntypedFormGroup({});
 
-	constructor() { }
+	vmCourses: Course[];
+
+	displayedColumns: string[] = ['holenum', 'g_a', 'b', 'w', 'g_b', 'r'];
+	dataSource: MatTableDataSource<TeeLocation>;
+
+	constructor(public teeService: TeeService, public courseService: CourseService)
+	{
+		this.dataSource = new MatTableDataSource<TeeLocation>();
+		this.dataSource.data = this.teeService.getTeeLocations();
+
+		this.vmCourses = courseService.getCourseList();
+	}
 
 	ngOnInit(): void
 	{
+		this.formGroup.addControl('CourseName', new UntypedFormControl());
+	}
+
+	onFilterCourse_Change(event: MatSelectChange)
+	{
+		this.dataSource.data = this.teeService.getTeeLocations().filter(tee =>
+		{
+			return tee.coursename === event.value;
+		}
+		);
 	}
 
 }
--- src/app/services/tee-service.ts
+++ src/app/services/tee-service.ts
@@ -11,6 +11,27 @@ export class TeeService
         , { id: 2, teebox_id: 3, color: 'Blue', enabled: true, coursename: 'Graywolf' }
     ];
 
+    private _testTeeLocations: TeeLocation[] = [
+        { id: 0, coursename: 'Graywolf', holeNum: 1, g_a: 1, b: 2, w: 3, g_b: 4, r: 5 }
+        , { id: 1, coursename: 'Graywolf', holeNum: 2, g_a: 1, b: 2, w: 3, g_b: 4, r: 5 }
+        , { id: 2, coursename: 'Graywolf', holeNum: 3, g_a: 1, b: 2, w: 3, g_b: 4, r: 5 }
+        , { id: 3, coursename: 'Graywolf', holeNum: 4, g_a: 1, b: 2, w: 3, g_b: 4, r: 5 }
+        , { id: 4, coursename: 'Graywolf', holeNum: 5, g_a: 1, b: 2, w: 3, g_b: 4, r: 5 }
+        , { id: 5, coursename: 'Graywolf', holeNum: 6, g_a: 1, b: 2, w: 3, g_b: 4, r: 5 }
+        , { id: 6, coursename: 'Graywolf', holeNum: 7, g_a: 1, b: 2, w: 3, g_b: 4, r: 5 }
+        , { id: 7, coursename: 'Graywolf', holeNum: 8, g_a: 1, b: 2, w: 3, g_b: 4, r: 5 }
+        , { id: 8, coursename: 'Graywolf', holeNum: 9, g_a: 1, b: 2, w: 3, g_b: 4, r: 5 }
+        , { id: 9, coursename: 'Graywolf', holeNum: 10, g_a: 1, b: 2, w: 3, g_b: 4, r: 5 }
+        , { id: 10, coursename: 'Graywolf', holeNum: 11, g_a: 1, b: 2, w: 3, g_b: 4, r: 5 }
+        , { id: 11, coursename: 'Graywolf', holeNum: 12, g_a: 1, b: 2, w: 3, g_b: 4, r: 5 }
+        , { id: 12, coursename: 'Graywolf', holeNum: 13, g_a: 1, b: 2, w: 3, g_b: 4, r: 5 }
+        , { id: 13, coursename: 'Graywolf', holeNum: 14, g_a: 1, b: 2, w: 3, g_b: 4, r: 5 }
+        , { id: 14, coursename: 'Graywolf', holeNum: 15, g_a: 1, b: 2, w: 3, g_b: 4, r: 5 }
+        , { id: 15, coursename: 'Graywolf', holeNum: 16, g_a: 1, b: 2, w: 3, g_b: 4, r: 5 }
+        , { id: 16, coursename: 'Graywolf', holeNum: 17, g_a: 1, b: 2, w: 3, g_b: 4, r: 5 }
+        , { id: 17, coursename: 'Graywolf', holeNum: 18, g_a: 1, b: 2, w: 3, g_b: 4, r: 5 }
+    ];
+
     private _id_index = 1;
 
     constructor()
@@ -26,6 +47,11 @@ export class TeeService
     getTees() 
     {
         return this._testTees;
+    }
+
+    getTeeLocations() 
+    {
+        return this._testTeeLocations;
     }
 
     getNewTeeID()
@@ -59,4 +85,16 @@ export interface Tee
     color: string;
     enabled: boolean;
     coursename: string;
+}
+
+export interface TeeLocation
+{
+    id: Number;
+    coursename: string;
+    holeNum: number;
+    g_a: number;
+    b: number;
+    w: number;
+    g_b: number;
+    r: number;
 }
